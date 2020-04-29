b0115f0d4cb199cf0f329e7a00d1e9bbd814ced3
LaurenzLuigi <laurenz.cruz@gmail.com>
2020-04-29 10:03:58 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-
 from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 import numpy as np
 
-setup(name='interp3d',
-      ext_modules=cythonize('interp3d/interp.pyx'),
+extension = [Extension(
+             'interp3d.interp',
+             ['interp3d/interp.pyx'],
+             extra_compile_args=['/openmp']            
+             )]
+
+setup(name='interp',
+      ext_modules=cythonize(extension,  compiler_directives={'language_level':'3'}),
       packages=['interp3d'],
       include_dirs=[np.get_include()])
